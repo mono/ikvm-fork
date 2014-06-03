4d031fcacdac13acb67d611255c2b987c350f2dc
jfrijters <jfrijters>
2014-06-03 08:56:30 +0000

    - Prevent unfocable windows from being activated by mouse click. - Fixed clipboard access to use the right thread. (Based on patch by Maria Papendieck of pure-systems.)

--- awt/toolkit-0.95.cs
+++ awt/toolkit-0.95.cs
@@ -127,6 +127,19 @@ namespace ikvm.awt
             }
         }
 
+		private const int WM_MOUSEACTIVATE = 0x0021;
+		private const int MA_NOACTIVATE = 0x0003;
+
+		protected override void WndProc(ref Message m)
+		{
+			if (!focusableWindow && m.Msg == WM_MOUSEACTIVATE)
+			{
+				m.Result = (IntPtr)MA_NOACTIVATE;
+				return;
+			}
+			base.WndProc(ref m);
+		}
+
 		protected override void OnPaintBackground(PaintEventArgs e)
 		{
 			NetComponentPeer peer = NetComponentPeer.FromControl(this);
@@ -280,7 +293,7 @@ namespace ikvm.awt
         private NetClipboard clipboard;
 		private bool eventQueueSynchronizationContext;
 
-		protected override java.awt.EventQueue getSystemEventQueueImpl()
+		protected internal override java.awt.EventQueue getSystemEventQueueImpl()
 		{
 			java.awt.EventQueue eq = base.getSystemEventQueueImpl();
 			if (!eventQueueSynchronizationContext)
@@ -329,7 +342,7 @@ namespace ikvm.awt
             return Environment.OSVersion.Platform == PlatformID.Win32NT || Environment.OSVersion.Platform == PlatformID.Win32Windows;
         }
 
-        protected override void loadSystemColors(int[] systemColors)
+        protected internal override void loadSystemColors(int[] systemColors)
         {
             // initialize all colors to purple to make the ones we might have missed stand out
             for (int i = 0; i < systemColors.Length; i++)
@@ -539,9 +552,9 @@ namespace ikvm.awt
             throw new NotImplementedException();
         }
 
-        public override java.awt.peer.KeyboardFocusManagerPeer createKeyboardFocusManagerPeer(java.awt.KeyboardFocusManager manager)
+        public override java.awt.peer.KeyboardFocusManagerPeer getKeyboardFocusManagerPeer()
         {
-            return new NetKeyboardFocusManagerPeer(manager);
+            return new NetKeyboardFocusManagerPeer();
         }
 
         public override java.awt.Dimension getScreenSize()
@@ -566,19 +579,6 @@ namespace ikvm.awt
         {
             //we return the default ColorModel because this produce the fewest problems with convertions
             return java.awt.image.ColorModel.getRGBdefault();
-        }
-
-        [Obsolete]
-        public override string[] getFontList()
-        {
-            // This method is deprecated and Sun's JDK only returns these fonts as well
-            return new string[] { "Dialog", "SansSerif", "Serif", "Monospaced", "DialogInput" };
-        }
-
-        [Obsolete]
-        public override java.awt.FontMetrics getFontMetrics(java.awt.Font font)
-        {
-            return sun.font.FontDesignMetrics.getMetrics(font);
         }
 
         public override void sync()
@@ -725,7 +725,7 @@ namespace ikvm.awt
             throw new NotImplementedException();
         }
 */
-        protected override java.awt.peer.DesktopPeer createDesktopPeer(java.awt.Desktop target)
+        protected internal override java.awt.peer.DesktopPeer createDesktopPeer(java.awt.Desktop target)
         {
             return new NetDesktopPeer();
         }
@@ -749,7 +749,7 @@ namespace ikvm.awt
 			}
         }
 
-        protected override void initializeDesktopProperties()
+        protected internal override void initializeDesktopProperties()
         {
             //copied from WToolkit.java
             desktopProperties.put("DnD.Autoscroll.initialDelay", java.lang.Integer.valueOf(50));
@@ -775,7 +775,7 @@ namespace ikvm.awt
             }
         }
 
-        protected override Object lazilyLoadDesktopProperty(String name)
+        protected internal override Object lazilyLoadDesktopProperty(String name)
         {
             if ("win.defaultGUI.font".Equals(name))
             {
@@ -785,18 +785,13 @@ namespace ikvm.awt
             return null;
         }
 
-        protected override java.awt.peer.MouseInfoPeer getMouseInfoPeer() {
+        protected internal override java.awt.peer.MouseInfoPeer getMouseInfoPeer() {
             return new NetMouseInfoPeer();
         }
 
         /*===============================
          * Implementations of interface IkvmToolkit
          */
-
-        public java.awt.Graphics2D createGraphics(System.Drawing.Bitmap bitmap)
-        {
-            return new BitmapGraphics(bitmap);
-        }
 
         /// <summary>
         /// Get a helper class for implementing the print API
@@ -888,12 +883,12 @@ namespace ikvm.awt
             return null;
         }
 
-        protected override int getScreenHeight()
+        protected internal override int getScreenHeight()
         {
             return Screen.PrimaryScreen.Bounds.Height;
         }
 
-        protected override int getScreenWidth()
+        protected internal override int getScreenWidth()
         {
             return Screen.PrimaryScreen.Bounds.Width;
         }
@@ -949,7 +944,7 @@ namespace ikvm.awt
             }
         }
         
-        protected override bool syncNativeQueue(long l)
+        protected internal override bool syncNativeQueue(long l)
         {
             throw new NotImplementedException();
         }
@@ -1035,6 +1030,11 @@ namespace ikvm.awt
 		public override bool areExtraMouseButtonsEnabled()
 		{
 			return true;
+		}
+
+		public override java.awt.peer.FramePeer createLightweightFrame(sun.awt.LightweightFrame lf)
+		{
+			throw new NotImplementedException();
 		}
 	}
 
@@ -1285,7 +1285,7 @@ namespace ikvm.awt
             dragStart = false;
         }
 
-        protected override void startDrag(java.awt.datatransfer.Transferable trans, long[] formats, Map formatMap)
+        protected internal override void startDrag(java.awt.datatransfer.Transferable trans, long[] formats, Map formatMap)
         {
             dragStart = true;
 
@@ -1340,7 +1340,7 @@ namespace ikvm.awt
             return 0;
         }
 
-        protected override void setNativeCursor(long nativeCtxt, java.awt.Cursor c, int cType)
+        protected internal override void setNativeCursor(long nativeCtxt, java.awt.Cursor c, int cType)
         {
             
         }
@@ -1415,7 +1415,7 @@ namespace ikvm.awt
                                      dropAction, actions, formats, nativeCtxt, eventID, dispatchType);
         }
 
-        protected override void doDropDone(bool success, int dropAction, bool isLocal)
+        protected internal override void doDropDone(bool success, int dropAction, bool isLocal)
         {
             // Don't do anything as .NET framework already handle the message pump
         }
@@ -1434,7 +1434,7 @@ namespace ikvm.awt
             return new NetClipboardTransferable(data).getTransferData(df);
         }
 
-        protected override object getNativeData(long l)
+        protected internal override object getNativeData(long l)
         {
             throw new NotImplementedException();
         }
@@ -2629,7 +2629,7 @@ namespace ikvm.awt
 				{
 					font = defaultFont;
 				}
-				return new ComponentGraphics(this.control, fgColor, bgColor, font);
+				return new ComponentGraphics(this.control, target, fgColor, bgColor, font);
 			}
 			return null;
 		}
@@ -3671,15 +3671,68 @@ namespace ikvm.awt
         }
 	}
 
-    sealed class NetTextAreaPeer : NetTextComponentPeer<java.awt.TextArea>, java.awt.peer.TextAreaPeer
+    sealed class NetTextAreaPeer : NetComponentPeer<java.awt.TextArea, RichTextBox>, java.awt.peer.TextAreaPeer
 	{
 		public NetTextAreaPeer(java.awt.TextArea textArea)
 			: base(textArea)
 		{
 			control.ReadOnly = !((java.awt.TextArea)target).isEditable();
 			control.WordWrap = false;
-			control.ScrollBars = ScrollBars.Both;
+			control.ScrollBars = RichTextBoxScrollBars.Both;
 			control.Multiline = true;
+			control.AutoSize = false;
+			control.Text = target.getText();
+		}
+
+		public override bool isFocusable()
+		{
+			return true;
+		}
+
+		public int getSelectionEnd()
+		{
+			return NetToolkit.Invoke<int>(delegate { return control.SelectionStart + control.SelectionLength; });
+		}
+
+		public int getSelectionStart()
+		{
+			return NetToolkit.Invoke<int>(delegate { return control.SelectionStart; });
+		}
+
+		public string getText()
+		{
+			return NetToolkit.Invoke<string>(delegate { return control.Text; });
+		}
+
+		public void setText(string text)
+		{
+			NetToolkit.Invoke(delegate { control.Text = text; });
+		}
+
+		public void select(int start_pos, int end_pos)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void setEditable(bool editable)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int getCaretPosition()
+		{
+			return getSelectionStart();
+		}
+
+		private void setCaretPositionImpl(int pos)
+		{
+			control.SelectionStart = pos;
+			control.SelectionLength = 0;
+		}
+
+		public void setCaretPosition(int pos)
+		{
+			NetToolkit.Invoke(setCaretPositionImpl, pos);
 		}
 
 		public void insert(string text, int pos)
@@ -3720,11 +3773,22 @@ namespace ikvm.awt
 
 		public void replaceRange(string text, int start_pos, int end_pos)
 		{
-			throw new NotImplementedException();
+			NetToolkit.Invoke(delegate { control.Text = control.Text.Substring(0, start_pos) + text + control.Text.Substring(end_pos); });
 		}
+
 		public void replaceText(string text, int start_pos, int end_pos)
 		{
+			replaceRange(text, start_pos, end_pos);
+		}
+
+		public java.awt.im.InputMethodRequests getInputMethodRequests()
+		{
 			throw new NotImplementedException();
+		}
+
+		protected sealed override RichTextBox CreateControl()
+		{
+			return new RichTextBox();
 		}
 	}
 
@@ -4071,7 +4135,7 @@ namespace ikvm.awt
 			return NetToolkit.Invoke<bool>(control.Focus);
 		}
 
-        public void setAlwaysOnTop(bool alwaysOnTop)
+        public void updateAlwaysOnTopState()
         {
             // The .NET property TopMost does not work with a not focusable Window
             // that we need to set the window flags directly. To reduce double code
@@ -4366,6 +4430,11 @@ namespace ikvm.awt
 		{
 			return new MyForm(_insets);
 		}
+
+		public void emulateActivation(bool b)
+		{
+			throw new NotImplementedException();
+		}
     }
 
     sealed class NetDialogPeer : NetWindowPeer, java.awt.peer.DialogPeer
@@ -4424,13 +4493,7 @@ namespace ikvm.awt
 
     sealed class NetKeyboardFocusManagerPeer : java.awt.peer.KeyboardFocusManagerPeer
     {
-        //private readonly java.awt.KeyboardFocusManager manager;
         private static java.lang.reflect.Method m_removeLastFocusRequest;
-
-        public NetKeyboardFocusManagerPeer(java.awt.KeyboardFocusManager manager)
-        {
-            //this.manager = manager;
-        }
 
         public void clearGlobalFocusOwner(java.awt.Window activeWindow)
         {
@@ -4449,6 +4512,10 @@ namespace ikvm.awt
         {
             return getNativeFocusedWindow();
         }
+
+		public void setCurrentFocusedWindow(java.awt.Window w)
+		{
+		}
 
 		private static java.awt.Component getNativeFocusOwner()
 		{
@@ -4503,9 +4570,17 @@ namespace ikvm.awt
             {
                 if (m_removeLastFocusRequest == null)
                 {
-                    m_removeLastFocusRequest = SunToolkit.getMethod(typeof(java.awt.KeyboardFocusManager), "removeLastFocusRequest",
-                                                                  new java.lang.Class[] { typeof(java.awt.Component) });
-                }
+					java.security.AccessController.doPrivileged(Delegates.toPrivilegedAction(delegate
+					{
+						java.lang.Class keyboardFocusManagerCls = typeof(java.awt.KeyboardFocusManager);
+						java.lang.reflect.Method method = keyboardFocusManagerCls.getDeclaredMethod(
+							"removeLastFocusRequest",
+							typeof(java.awt.Component));
+						method.setAccessible(true);
+						m_removeLastFocusRequest = method;
+						return null;
+					}));
+				}
                 m_removeLastFocusRequest.invoke(null, new Object[] { heavyweight });
             }
             catch (java.lang.reflect.InvocationTargetException ite)
@@ -5238,7 +5313,7 @@ namespace ikvm.awt
             {
                 return contents;
             }
-            return new NetClipboardTransferable(Clipboard.GetDataObject());
+            return new NetClipboardTransferable(NetToolkit.Invoke<IDataObject>(Clipboard.GetDataObject));
         }
     }
 
@@ -5506,12 +5581,12 @@ namespace ikvm.awt
             return obj;
         }
 
-        protected override string getClipboardFormatName(long format)
+        protected internal override string getClipboardFormatName(long format)
         {
             return getNativeClipboardFormatName(format);
         }
 
-        protected override byte[] imageToStandardBytes(java.awt.Image image, string mimeType)
+        protected internal override byte[] imageToStandardBytes(java.awt.Image image, string mimeType)
         {
             if (image is NoImage) return null;
             Image netImage = J2C.ConvertImage(image);
@@ -5548,10 +5623,15 @@ namespace ikvm.awt
             return handler;
         }
 
-        protected override java.io.ByteArrayOutputStream convertFileListToBytes(java.util.ArrayList fileList)
+        protected internal override java.io.ByteArrayOutputStream convertFileListToBytes(java.util.ArrayList fileList)
         {
             throw new ikvm.@internal.NotYetImplementedError();
         }
-    }
+
+		protected internal override java.awt.Image platformImageBytesToImage(byte[] barr, long l)
+		{
+			throw new NotImplementedException();
+		}
+	}
 
 }
